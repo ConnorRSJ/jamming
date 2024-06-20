b3682404239f10d28dade2ce5a6a2dba6e5a44ea
Connor Jeynes <connor.rsj@gmail.com>
2024-06-20 01:48:45 +0100

    Added styles to Playlist.js

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Tracklist from "../Tracklist/Tracklist";
+import styles from "./Playlist.module.css";
 
 function Playlist() {
     return (
-        <div className="Playlist">
+        <div className={styles.Playlist}>
             <input defaultValue={"New Playlist"} />
             <Tracklist />
-            <button className="PlaylistSave">SAVE TO SPOTIFY</button>
+            <button className={styles.PlaylistSave}>SAVE TO SPOTIFY</button>
         </div>
     );
 }
